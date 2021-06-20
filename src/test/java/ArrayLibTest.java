547a58d4395b852c339da3a4419772f3e0e1f12b
andreysc1976 <andreysc@yandex.ru>
2021-06-20 20:17:37 +0300

    Добавлены комментарии

--- src/test/java/ArrayLibTest.java
+++ src/test/java/ArrayLibTest.java
@@ -18,10 +18,10 @@ class ArrayLibTest {
 
     private static Stream<Arguments> arrayForNextFourTests(){
         return Stream.of(
-                Arguments.of(new int[]{1,2,3,1,2,3},new int[]{2,4,3,4,1,2,3,1,2,3}),
-                Arguments.of(new int[]{1,2,2,3},new int[]{4,1,2,2,3}),
-                Arguments.of(new int[]{1},new int[]{1,2,3,4,1}),
-                Arguments.of(new int[]{},new int[]{1,2,3,4})
+                Arguments.of(new int[]{1,2,3,1,2,3},new int[]{2,4,3,4,1,2,3,1,2,3}), //тест на четыре в "середине" массива
+                Arguments.of(new int[]{1,2,2,3},new int[]{4,1,2,2,3}), //тест 4 на первом месте и одна
+                Arguments.of(new int[]{1},new int[]{1,2,3,4,1}), //тест на 4 на предпоследнем месте, когда в результате массив из одного элеменьа
+                Arguments.of(new int[]{},new int[]{1,2,3,4}) //тест на 4 на последем месте, когда резултьтат массив нулевой длины
         );
     }
 
@@ -40,12 +40,13 @@ class ArrayLibTest {
 
     private static Stream<Arguments> parametrsForCheckForOneAndFour(){
         return Stream.of(
-                Arguments.of(true,new int []{1,2,3,4,1,1,2,2,1,4}),
-                Arguments.of(true,new int []{2,2,2,1,4,2,2,2,2}),
-                Arguments.of(true,new int []{4,2,2,3,2,3,1}),
-                Arguments.of(true,new int []{1,2,2,3,3,5,6,8}),
-                Arguments.of(true,new int []{4}),
-                Arguments.of(false,new int []{})
+                Arguments.of(true,new int []{1,2,3,4,1,1,2,2,1,4}),//обычный вариант
+                Arguments.of(true,new int []{2,2,2,1,4,2,2,2,2}),//1 и 4 рядом в центре
+                Arguments.of(true,new int []{4,2,2,3,2,3,1}),//4 в начале, 1 в конце
+                Arguments.of(true,new int []{1,2,2,3,3,5,6,8}), //одно значение в начале
+                Arguments.of(false,new int []{5,2,2,3,3,5,6,8}), //ни одного значения в массиве
+                Arguments.of(true,new int []{4}),//только одно правильное значение
+                Arguments.of(false,new int []{})//пустой массив
 
         );
     }
